0e653537d7888b84bee093b76eec1a7429e4d83e
ohne <ghdlr785@gmail.com>
2022-06-16 13:32:01 +0900

    초기 커밋.

---
+++ cote3/cote3.cpp
@@ -0,0 +1,23 @@
+﻿#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    int i = 0;
+    for (i; i < phone_number.size() - 4; i++)
+    {
+        answer += '*';
+    }
+    for (i; i < phone_number.size(); i++)
+    {
+        answer += phone_number[i];
+    }
+    return answer;
+}
+
+int main()
+{
+
+}
